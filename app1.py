446751a4ff43e40318f4675feff38030b4dd7cbc
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-03-15 13:54:35 +0530

    Random Password

--- app1.py
+++ app1.py
@@ -1,38 +1,10 @@
 import random 
 import string
 
-def generate_password(min_length, numbers=False, special_characters=False):
-   letters=string.ascii_letters
-   digits=string.digits  
-   special=string.punctuation
-
-   characters=letters
-   if numbers:
-      characters+=digits
-   if special_characters:
-      characters+=special   
-      
-   password=""
-   
-   while min_length< or min_length>50:
-        if min_length==max(min_length, 50):
-           min_length=int(input())
-           print("Enter min_length of the password: ")
-           return generate_password(min_length, numbers, special_characters)
-        elif characters==numbers:
-              print("Include numbers? (y/n): ")
-              include_numbers=int(input())
-              if include_numbers==1:
-                 characters+=digits
-        elif characters==special:
-              print("Include special characters? (y/n): ")
-              include_special=int(input())
-              if include_special==1:
-                 characters+=special
-        else:
-            break
-        for i in range(min_length):
-            password+=random.choice(characters)
-        return password
-password=generate_password(8)
-print("Generated password: ",password)
+if __name__=="__main__":
+   s1=string.ascii_letters
+   print(s1)
+   s2=string.digits
+   print(s2)
+   s3=string.punctuation
+   print(s3)
